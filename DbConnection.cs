40ef2caff2ab878f20edaa9d1502bb523264f83a
YevhenPuzenko <blockshlako@gmail.com>
2022-10-18 23:44:34 +0200

    add db connection and appsettings

---
+++ DbConnection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCIS_2
+{
+    internal class DbConnection :DbContext
+    {
+        public DbConnection(DbContextOptions<DbConnection> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Author> Authors { get; set; } = null!;
+        public DbSet<Book> Books { get; set; } = null!;
+        public DbSet<BookOutOfStorage> BooksOutOfStorage { get; set; } = null!;
+        public DbSet<LibraryUser> LibraryUsers { get; set; } = null!;
+        public DbSet<Person> People { get; set; } = null!;
+        public DbSet<Storage> Storages { get; set; } = null!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PCIS_2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var conf = new ConfigurationBuilder()
+            .AddJsonFile("C:\\Users\\KedrAdmin\\source\\repos\\PCIS-2\\appsettings.json").Build();
+
+        var optionBuilder = new DbContextOptionsBuilder<DbConnection>();
+        var libraryDB = new DbConnection(optionBuilder.UseSqlServer(conf.GetConnectionString("Library")).Options);
+        
+
+
+    }
+}
